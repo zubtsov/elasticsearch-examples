7d2cbe33ccbe97cfebfc9b4d95ab083d0c96d0fb
Ruslan_Zubtsov <Ruslan_Zubtsov@epam.com>
2018-02-13 23:16:00 +0300

    Performed refactoring.

--- mail-searcher/src/main/resources/elastic_script.js
+++ mail-searcher/src/main/resources/elastic_script.js
@@ -1,59 +1,44 @@
+var tableHeaders = ["Order", "Folder", "From", "Subject", "Sent date", "Received date", "Recipients", "Reply to"];
+var fieldNames = ["Order", "Folder", "From", "Subject", "Sent date", "Received date", "Recipients", "Reply to"];
+
 function loadDocs() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var response = JSON.parse(this.responseText);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
 
-      var table = document.createElement("TABLE");
-      var header = table.createTHead();
-      var row = header.insertRow(0);
-      row.insertCell().innerHTML="<b>Order</b>";
-      row.insertCell().innerHTML="<b>Folder</b>";
-      row.insertCell().innerHTML="<b>From</b>";
-      row.insertCell().innerHTML="<b>Subject</b>";
-      row.insertCell().innerHTML="<b>Sent date</b>";
-      row.insertCell().innerHTML="<b>Received date</b>";
-      row.insertCell().innerHTML="<b>Recipients</b>";
-      row.insertCell().innerHTML="<b>Reply to</b>";
+            var table = document.createElement("TABLE");
+            var header = table.createTHead();
+            var row = header.insertRow(0);
+            for (i = 0; i<tableHeaders.length; i++) {
+            row.insertCell().innerHTML=tableHeaders[i];
+        }
 
-      var hit;
-      for (i = 0; i < response.hits.hits.length; i++) {
-        hit = response.hits.hits[i];
+        var hit;
+        for (i = 0; i < response.hits.hits.length; i++) {
+            hit = response.hits.hits[i];
 
-        var row = table.insertRow();
-        var numberCell = row.insertCell();
-        var folderCell = row.insertCell();
-        var fromCell = row.insertCell();
-        var subjectCell = row.insertCell();
-        var sentDateCell = row.insertCell();
-        var receivedDateCell = row.insertCell();
-        var recipientsCell = row.insertCell();
-        var replyToCell = row.insertCell();
+            table.insertRow();
+            row.insertCell().innerHTML=i;
+            for (j = 0; j < fieldNames.length; j++) {
+                row.insertCell().innerHTML=escapeHtml(hit._source[fieldNames[j]].toString());
+            }
+        }
 
-        numberCell.innerHTML=i;
-
-        folderCell.innerHTML=hit._source.Folder;
-        fromCell.innerHTML=escapeHtml(hit._source.From.toString());
-        subjectCell.innerHTML=hit._source.Subject;
-        sentDateCell.innerHTML=hit._source["Sent date"];
-        receivedDateCell.innerHTML=hit._source["Received Date"];
-        recipientsCell.innerHTML=escapeHtml(hit._source.Recipients.toString());
-        replyToCell.innerHTML=escapeHtml(hit._source["Reply to"].toString());
-      }
-
-      document.getElementById("results").innerHTML="";
-      document.getElementById("results").append(table);
-    }
-  };
-  function escapeHtml(unsafe) {
-    return unsafe
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
-  };
-  xhttp.open("POST", document.getElementById("url").value, true);
-  xhttp.setRequestHeader("Content-Type", "application/json");
-  xhttp.send(document.getElementById("query").value);
+        document.getElementById("results").innerHTML="";
+        document.getElementById("results").append(table);
+        }
+    };
+    xhttp.open("POST", document.getElementById("url").value, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(document.getElementById("query").value);
 }
+
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+};
--- mail-searcher/src/main/resources/solr_script.js
+++ mail-searcher/src/main/resources/solr_script.js
@@ -1,58 +1,46 @@
+var tableHeaders = ["Order", "Folder", "From", "Subject", "Sent date", "Received date", "Recipients", "Reply to"];
+var fieldNames = ["folder", "from", "subject", "sent_date", "received_date", "recipients", "reply_to"];
+
 function loadDocs() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var response = JSON.parse(this.responseText);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
 
-      var table = document.createElement("TABLE");
-      var header = table.createTHead();
-      var row = header.insertRow(0);
-      row.insertCell().innerHTML="<b>Order</b>";
-      row.insertCell().innerHTML="<b>Folder</b>";
-      row.insertCell().innerHTML="<b>From</b>";
-      row.insertCell().innerHTML="<b>Subject</b>";
-      row.insertCell().innerHTML="<b>Sent date</b>";
-      row.insertCell().innerHTML="<b>Received date</b>";
-      row.insertCell().innerHTML="<b>Recipients</b>";
-      row.insertCell().innerHTML="<b>Reply to</b>";
+            var table = document.createElement("TABLE");
+            var header = table.createTHead();
+            var row = header.insertRow(0);
+            for (i = 0; i<tableHeaders.length; i++) {
+                row.insertCell().innerHTML=tableHeaders[i];
+            }
 
-      var hit;
-      for (i = 0; i < response.response.docs.length; i++) {
-        hit = response.response.docs[i];
+            var hit;
+            for (i = 0; i < response.response.docs.length; i++) {
 
-        var row = table.insertRow();
-        var numberCell = row.insertCell();
-        var folderCell = row.insertCell();
-        var fromCell = row.insertCell();
-        var subjectCell = row.insertCell();
-        var sentDateCell = row.insertCell();
-        var receivedDateCell = row.insertCell();
-        var recipientsCell = row.insertCell();
-        var replyToCell = row.insertCell();
+                hit = response.response.docs[i];
 
-        numberCell.innerHTML=i;
+                var row = table.insertRow();
+                row.insertCell().innerHTML=i;
 
-        folderCell.innerHTML=hit.folder;
-        fromCell.innerHTML=escapeHtml(hit.from.toString());
-        subjectCell.innerHTML=hit.subject;
-        sentDateCell.innerHTML=hit.sent_date;
-        receivedDateCell.innerHTML=hit.received_date;
-        recipientsCell.innerHTML=escapeHtml(hit.recipients.toString());
-        replyToCell.innerHTML=escapeHtml(hit.reply_to.toString());
-      }
+                for (j = 0; j<fieldNames.length; j++) {
+                    row.insertCell().innerHTML=escapeHtml(hit[fieldNames[j]].toString());
+                }
+            }
 
-      document.getElementById("results").innerHTML="";
-      document.getElementById("results").append(table);
-    }
-  };
-  function escapeHtml(unsafe) {
-    return unsafe
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
-  };
-  xhttp.open("GET", document.getElementById("url").value, true);
-  xhttp.send();
+            document.getElementById("results").innerHTML="";
+            document.getElementById("results").append(table);
+        }
+    };
+
+    xhttp.open("GET", document.getElementById("url").value, true);
+    xhttp.send();
 }
+
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+};
